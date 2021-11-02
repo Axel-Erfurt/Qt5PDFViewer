f9a0b29c90d6a3fab43b0dfdc779afd5e81c29b6
Axel Schneider <axel99092@t-online.de>
2021-11-02 23:43:43 +0100

    Update myPDFViewer.py

--- myPDFViewer.py
+++ myPDFViewer.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import QApplication
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 
+root = os.path.dirname(sys.argv[0])
 PDFJS = f"file://{os.path.abspath('./web/viewer.html')}"
 print(PDFJS)
 PDF = f'file://{"%20".join(sys.argv[1:])}'
@@ -14,7 +15,10 @@ print("loading PDF:", PDF)
 class Window(QWebEngineView):
     def __init__(self):
         super(Window, self).__init__()
-        self.load(QUrl.fromUserInput(f'{PDFJS}?file={PDF}'))
+        if len(sys.argv) > 1:
+            self.load(QUrl.fromUserInput(f'{PDFJS}?file={PDF}'))
+        else:
+            self.load(QUrl.fromUserInput(f"{PDFJS}?file=file://{root}/test.pdf"))
 
 if __name__ == '__main__':
 
