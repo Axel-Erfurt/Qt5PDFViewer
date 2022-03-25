f95b5f78d6eb8eddd9e6ccc075263e6d86d9ce8f
Axel Schneider <axel99092@t-online.de>
2022-03-25 18:36:41 +0100

    Create PDFViewer_PyQt6.py
    added PyQt6 Version

---
+++ PDFViewer_PyQt6.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PyQt6.QtCore import QUrl
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt6.QtWebEngineWidgets import QWebEngineView #, QWebEngineSettings
+from os import path
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(QMainWindow, self).__init__()
+
+        self.setWindowTitle("PDF Viewer")
+        self.setGeometry(0, 28, 1000, 750)
+
+        self.webView = QWebEngineView()
+        self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PluginsEnabled, True)
+        self.webView.settings().setAttribute(self.webView.settings().WebAttribute.PdfViewerEnabled, True)
+        self.setCentralWidget(self.webView)
+
+    def url_changed(self):
+        self.setWindowTitle(self.webView.title())
+
+    def go_back(self):
+        self.webView.back()
+
+if __name__ == '__main__':
+
+    import sys
+    app = QApplication(sys.argv)
+    win = MainWindow()
+    win.show()
+    if len(sys.argv) > 1:
+        win.webView.setUrl(QUrl(f"file://{sys.argv[1]}"))
+    else:
+        wd = path.dirname(sys.argv[0])
+        test_pdf = "test.pdf"
+        win.webView.setUrl(QUrl(f"file://{wd}/{test_pdf}"))
+    sys.exit(app.exec())
